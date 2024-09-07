6d9c2aa85fa21a121d4ec4beeb06fd0987673505
gianlucafiore <gfiorepersonal@gmail.com>
2024-09-07 00:38:30 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const time = {
+  name: "Time",
+  sql_name: "time precision",
+  fieldviews: {
+    show: { isEdit: false, run: (s) => `${s}` },
+    edit: {
+      isEdit: true,
+      run: (nm, v, attrs, cls) =>
+        `<input type="time" class="form-control ${cls}" name="${nm}" id="input${nm}" ${
+          v || v === 0 ? `value="${v}"` : ""
+        }>`,
+    },
+  },
+  read: (v) => {
+    switch (typeof v) {
+      case "number":
+        return v;
+      case "string":
+        const parsed = parseFloat(v);
+        return isNaN(parsed) ? undefined : parsed;
+      default:
+        return undefined;
+    }
+  },
+};
+
+module.exports = { sc_plugin_api_version: 1, types: [time] };
