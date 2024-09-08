d051658c454b71bbf54b0a21a1ecfaf17af945ef
gianlucafiore <gfiorepersonal@gmail.com>
2024-09-08 18:29:09 -0300

    console

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ const time = {
     show: { isEdit: false, run: (s) => `${s}` },
     edit: {
       isEdit: true,
-      run: (nm, v, attrs, cls) =>
-        `<input type="time" class="form-control ${cls}" name="${nm}" id="input${nm}" ${
+      run: (nm, v, attrs, cls) =>{
+        console.log(v)
+        return `<input type="time" class="form-control ${cls}" name="${nm}" id="input${nm}" ${
           v || v === 0 ? `value="${v}"` : ""
-        }>`,
+        }>`
+      },
     },
   },
   read: (v) => {
